2f4b0f85b864612c98f22bc22eb66c32420f19fa
Emaduwage Thaveesha Urvinda <thaveeshaurvinda2007@gmail.com>
2026-03-22 19:11:48 +0530

    Implemented the corner rectangle to show bounding box

--- Testing_Yolo/webcam_yolo.py
+++ Testing_Yolo/webcam_yolo.py
@@ -39,6 +39,9 @@ while True:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
             # Class Name
