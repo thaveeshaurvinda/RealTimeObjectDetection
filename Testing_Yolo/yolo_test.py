5d0ac70d440c4eaf49ceaf5aad798c27e38748ce
Emaduwage Thaveesha Urvinda <thaveeshaurvinda2007@gmail.com>
2026-03-22 18:59:54 +0530

    Tested yolov8l on images

--- Testing_Yolo/yolo_test.py
+++ Testing_Yolo/yolo_test.py
@@ -6,7 +6,7 @@ torch.load = functools.partial(torch.load, weights_only=False)
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('../Yolo-Weights/yolov8n.pt')
+model = YOLO('../Yolo-Weights/yolov8l.pt')
 results = model("Images/3.jpg", show = True)
 cv2.waitKey(0)
 
